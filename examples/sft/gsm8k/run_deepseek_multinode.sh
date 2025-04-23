3488a85191576ec610b90b11777c02cd37976270
sguo35 <sguo35@gmail.com>
2025-04-23 02:47:54 +0000

    add test multi node

---
+++ examples/sft/gsm8k/run_deepseek_multinode.sh
@@ -0,0 +1,39 @@
+set -x
+
+if [ "$#" -lt 2 ]; then
+    echo "Usage: run_deepseek_6b7.sh <nproc_per_node> <save_path> [other_configs...]"
+    exit 1
+fi
+
+nproc_per_node=$1
+node_rank=$2
+save_path=$3
+
+# Shift the arguments so $@ refers to the rest
+shift 3
+
+
+torchrun   --nproc_per_node=$nproc_per_node \
+    --nnodes=2 \
+    --node_rank=$node_rank \
+    --master_addr=10.65.0.2 \
+    --master_port=29400 \
+     -m verl.trainer.fsdp_sft_trainer \
+    data.train_files=$HOME/data/gsm8k/train.parquet \
+    data.val_files=$HOME/data/gsm8k/test.parquet \
+    data.prompt_key=extra_info \
+    data.response_key=extra_info \
+    +data.prompt_dict_keys=['question'] \
+    data.train_batch_size=256 \
+    +data.response_dict_keys=['answer'] \
+    data.micro_batch_size_per_gpu=4 \
+    model.partial_pretrain=deepseek-ai/deepseek-coder-6.7b-instruct \
+    model.lora_rank=256 \
+    model.lora_alpha=256 \
+    model.target_modules=[q_proj,k_proj,v_proj,o_proj,gate_proj,up_proj,down_proj] \
+    trainer.default_local_dir=$save_path \
+    trainer.project_name=gsm8k-sft \
+    trainer.experiment_name=gsm8k-sft-deepseek-coder-6.7b-instruct \
+    trainer.total_epochs=4 \
+    trainer.logger=['console'] \
+    trainer.default_hdfs_dir=null $@
