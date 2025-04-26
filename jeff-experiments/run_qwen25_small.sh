dabb1f88525bcdb381dc60c9231aec1eada7ff09
sguo35 <sguo35@gmail.com>
2025-04-25 18:47:40 -0700

    [infra] - add initial job dispatch

---
+++ jeff-experiments/run_qwen25_small.sh
@@ -0,0 +1,46 @@
+set -x
+
+if [ "$#" -lt 1 ]; then
+    echo "Usage: run_deepseek_6b7.sh <save_path> [other_configs...]"
+    exit 1
+fi
+
+save_path=$1
+
+# Shift the arguments so $@ refers to the rest
+shift 1
+
+MASTER_ADDR=$(echo "$SKYPILOT_NODE_IPS" | head -n1)
+
+torchrun    --nproc_per_node=$SKYPILOT_NUM_GPUS_PER_NODE \
+  --nnodes=$SKYPILOT_NUM_NODES \
+  --node_rank=$SKYPILOT_NODE_RANK \
+  --master_addr=$MASTER_ADDR \
+  --master_port=8008 \
+     -m verl.trainer.fsdp_sft_trainer \
+    data.train_files=$HOME/sky_workdir/mmlu_full_gpt41/train.parquet \
+    data.val_files=$HOME/sky_workdir/mmlu_full_gpt41/train.parquet \
+    data.prompt_key=extra_info \
+    data.response_key=extra_info \
+    +data.prompt_dict_keys=['question'] \
+    +data.response_dict_keys=['answer'] \
+    data.train_batch_size=32 \
+    data.micro_batch_size_per_gpu=16 \
+    data.max_length=8192 \
+    data.multiturn.enable=True \
+    model.partial_pretrain=Qwen/Qwen2.5-1.5B-Instruct \
+    model.enable_gradient_checkpointing=True \
+    model.fsdp_config.wrap_policy.min_num_params=20000000 \
+    ulysses_sequence_parallel_size=2 \
+    use_remove_padding=True \
+    model.use_liger=True \
+    model.lora_rank=256 \
+    model.lora_alpha=256 \
+    model.target_modules=[q_proj,k_proj,v_proj,o_proj,gate_proj,up_proj,down_proj] \
+    trainer.default_local_dir=$save_path \
+    trainer.project_name=$PROJECT_NAME \
+    trainer.experiment_name=$EXPERIMENT_NAME \
+    trainer.total_epochs=2 \
+    trainer.logger=['console','wandb'] \
+    trainer.total_training_steps=4 \
+    trainer.default_hdfs_dir=null $@
